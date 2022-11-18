7656411b5530c72b120aeb15fe852a3692e6b08a
AprilRoseMontiadora <116123112+AprilRoseMontiadora@users.noreply.github.com>
2022-11-18 18:07:31 +0800

    Update Cellphone.java

--- Cellphone.java
+++ Cellphone.java
@@ -1,3 +1,20 @@
 public class Cellphone
 {
+	private String model; // attribute
+	private Cellphone camera; //component or part
+	
+	//constructor
+	public Cellphone(String model){
+		this.model=model;
+	}
+
+   //accessor
+   public String getModel(){
+	   return model;
+}
+   
+   //mutator
+   public void setModel(String model){
+	   
+   }
 }

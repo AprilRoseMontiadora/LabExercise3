c670eeb561249e85b684af412445fdf2b3323075
AprilRoseMontiadora <116123112+AprilRoseMontiadora@users.noreply.github.com>
2022-11-18 18:05:20 +0800

    Update Main.java

--- Main.java
+++ Main.java
@@ -4,17 +4,10 @@ public class Main
 {
 	public static void main(String[] args)
 	{
-		System.out.println("Hello World!");
-
-		Scanner input = new Scanner(System.in);
-
-		System.out.print("Enter a number: ");
-		double number1 = input.nextDouble();
-
-		System.out.print("Enter second number: ");
-		double number2 = input.nextDouble();
-
-		double product = number1 * number2;
-		System.out.printf("The product of both numbers is: %f", product);
+		Cellphone vivo = new Cellphone("Vivo V21 Series");
+		Cellphone specs = new Cellphone("44MP ois Night Selfie");
+		
+		
+		System.out.println("" + vivo.getModel() + " " + specs.getModel());
 	}
 }

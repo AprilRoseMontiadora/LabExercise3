b9edd70fef407fa78a29ffc4ad6df56c609d2503
AprilRoseMontiadora <116123112+AprilRoseMontiadora@users.noreply.github.com>
2022-11-18 18:09:02 +0800

    Update camera.java

--- camera.java
+++ camera.java
@@ -1,3 +1,27 @@
 public class camera
 {
+	private String specs; //attribute
+	
+ // constructor
+ public camera (){
+	 specs="44MP ois Night Selfie";
+	 }
+	 
+//accessor
+public String getSpecs(){
+	return specs;
+}
+
+//mutator
+public void setSpecs(String specs){
+     this.specs=specs;
+}
+
+public String openCamera(){
+	return"The Cellphone is capturing.";
+}
+
+public String closeCamera(){
+	return"The cellphone camera is closing,";
+}
 }
